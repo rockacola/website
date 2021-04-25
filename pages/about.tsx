7c4f1d8d67b356535718a4e96377797719f9a697
Travis <travis@localhost.dev>
2021-04-26 01:38:26 +1000

    Adapt Tailwind

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p className={`container bg-red-100 rounded p-4`}>This is the about page</p>
     <p>
       <Link href="/">
         <a>Go home</a>
