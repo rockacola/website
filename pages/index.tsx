88b010023b7d28d88fa9379a9c91193b4b63da6b
Travis <travis@localhost.dev>
2021-04-26 02:05:07 +1000

    Implement homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Image from 'next/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <div className={`h-screen relative`} style={{ backgroundColor: '#e8e6ea' }}>
+      <div
+        className={`absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full border-8 border-pink-300 opacity-80 transition duration-700 hover:opacity-90`}
+        style={{ width: 120, height: 120 }}
+      >
+        <Image src="/images/user.png" alt="" width="120" height="120" />
+      </div>
+    </div>
   </Layout>
 )
 
